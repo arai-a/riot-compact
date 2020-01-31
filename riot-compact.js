698946d18ef6a9ddd45ff6b6f900af5eaede41d7
Tooru Fujisawa <arai_a@mac.com>
2020-02-01 06:17:40 +0900

    Some refactoring, more comments

--- riot-compact.js
+++ riot-compact.js
@@ -14,7 +14,18 @@ async function waitForClassName(name) {
 }
 
 // Task that can be run multiple times, but subsequent run within `timeout`
-// is folded into single run, after `timeout`.
+// is folded into single run, after `timeout` from the previous run.
+//
+//     run()  run()  run()                   run()
+//       |      |      |                       |
+//       v      v      v                       v
+// ------+---------------+---------------+-----+------> t
+//       |    timeout    |    timeout    |     |
+//       |<------------->|<------------->|     |
+//       |               |                     |
+//       v               v                     v
+//      f()             f()                   f()
+//
 class Task {
   constructor(f, timeout) {
     this.f = f;
@@ -46,14 +57,17 @@ class Task {
   }
 }
 
+// Class to track observers for adding childNode to given node.
 class AddedNodeObservers {
   constructor() {
     this.observers = [];
   }
 
+  // Add observer to `node`.
+  // When childNode is added to `node`, `callback` is called.
   add(node, callback) {
     const obs = new MutationObserver(mutationsList => {
-      if (this.hasAddedNodes(mutationsList)) {
+      if (this._hasAddedNodes(mutationsList)) {
         callback().catch(e => console.error(e));
       }
     });
@@ -64,6 +78,7 @@ class AddedNodeObservers {
     });
   }
 
+  // Disconnect all observers.
   disconnectAll() {
     for (const obs of this.observers) {
       try {
@@ -74,13 +89,84 @@ class AddedNodeObservers {
     this.observers = [];
   }
 
-  hasAddedNodes(mutationsList) {
+  _hasAddedNodes(mutationsList) {
     for (const item of mutationsList) {
       if (item.addedNodes.length) {
         return true;
       }
     }
     return false;
+  }
+}
+
+// Set document.body class for light/dark theme, to use from CSS.
+class ThemeToClass {
+  constructor() {
+    this.links = [];
+    this.observer = null;
+  }
+
+  async apply() {
+    // Wait for some random element to wait for link elements.
+    await waitForClassName("mx_MatrixChat");
+
+    this.collectLinks();
+    this.hookThemeChange();
+    this.updateTheme();
+  }
+
+  // Collect link elements for light/dark stylesheets.
+  collectLinks() {
+    for (const link of document.getElementsByTagName("link")) {
+      if (link.getAttribute("rel") !== "stylesheet") {
+        continue;
+      }
+
+      const title = link.getAttribute("title");
+      if (!title) {
+        continue;
+      }
+
+      const isLight = title.startsWith("Light");
+      const isDark = title.startsWith("Dark");
+      if (isLight || isDark) {
+        this.links.push({ node: link, isLight });
+      }
+    }
+  }
+
+  // When theme is changed, "disabled" attribute of link elements gets changed.
+  // Hook it and update class.
+  hookThemeChange() {
+    this.observer = new MutationObserver(() => {
+      this.updateTheme();
+    });
+
+    for (const link of this.links) {
+      this.observer.observe(link.node, {
+        attributes: true,
+      });
+    }
+  }
+
+  // Set document.body class for light/dark theme.
+  updateTheme() {
+    if (this.isLightTheme()) {
+      document.body.classList.remove("ext-body-dark");
+      document.body.classList.add("ext-body-light");
+    } else {
+      document.body.classList.remove("ext-body-light");
+      document.body.classList.add("ext-body-dark");
+    }
+  }
+
+  isLightTheme() {
+    for (const link of this.links) {
+      if (link.node.hasAttribute("disabled")) {
+        continue;
+      }
+      return link.isLight;
+    }
   }
 }
 
@@ -98,12 +184,12 @@ class TimelineModifier {
 
     this.observers = new AddedNodeObservers();
 
-    this.updateTask = new Task(async () => {
+    this.newTimelineItemTask = new Task(async () => {
       this.modifyTimeline(false);
     }, 500);
 
     this.roomSwitchTask = new Task(async () => {
-      this.clearOddEvenMap();
+      this.oddEvenMap = {};
       this.observers.disconnectAll();
       await this.update(false);
     }, 500);
@@ -114,8 +200,7 @@ class TimelineModifier {
     }, 500);
   }
 
-  async run() {
-    await this.hookStyleChange();
+  async apply() {
     await this.update(true);
   }
 
@@ -123,70 +208,22 @@ class TimelineModifier {
     this.list = await waitForClassName("mx_RoomView_MessageList");
     this.modifyTimeline(isFirst);
 
-    this.hookTimelineModification();
+    this.hookNewTimelineItem();
     await this.hookRoomSwitch();
     await this.hookTimelineReconstruct();
   }
 
-  // ==== Hook light/dark  ====
-
-  async hookStyleChange() {
-    // Wait for some random element to wait for link elements.
-    await waitForClassName("mx_MatrixChat");
-
-    const links = [];
-    function updateTheme() {
-      for (const link of links) {
-        if (link.node.hasAttribute("disabled")) {
-          continue;
-        }
-
-        if (link.isLight) {
-          document.body.classList.remove("ext-body-dark");
-          document.body.classList.add("ext-body-light");
-        } else {
-          document.body.classList.remove("ext-body-light");
-          document.body.classList.add("ext-body-dark");
-        }
-        break;
-      }
-    }
-
-    const obs = new MutationObserver(updateTheme);
-
-    for (const link of document.getElementsByTagName("link")) {
-      if (link.getAttribute("rel") !== "stylesheet") {
-        continue;
-      }
-
-      const title = link.getAttribute("title");
-      if (!title) {
-        continue;
-      }
-
-      const isLight = title.startsWith("Light");
-      const isDark = title.startsWith("Dark");
-      if (isLight || isDark) {
-        links.push({ node: link, isLight });
-        obs.observe(link, {
-          attributes: true,
-        });
-      }
-    }
-
-    updateTheme();
-  }
-
   // ==== Hook modification to timeline DOM  ====
 
-  hookTimelineModification() {
+  // Hook new timeline item events, including new message, scroll up and down.
+  hookNewTimelineItem() {
     this.observers.add(this.list, async () => {
-      this.updateTask.run();
+      this.newTimelineItemTask.run();
     });
   }
 
+  // Hook room switch that replaces timeline node.
   async hookRoomSwitch() {
-    // When switching room, all nodes gets replaced.
     const node = await waitForClassName("mx_MatrixChat");
     this.observers.add(node, async () => {
       if (!await this.isListAlive()) {
@@ -195,8 +232,9 @@ class TimelineModifier {
     });
   }
 
+  // In some case (maybe on scroll down), timeline content gets replaced.
+  // Hook to it and update the internal references etc.
   async hookTimelineReconstruct() {
-    // In some case (maybe on scroll down), timeline content gets replaced.
     const node = await waitForClassName("mx_RoomView_timeline");
     this.observers.add(node, async () => {
       if (!await this.isListAlive()) {
@@ -219,11 +257,14 @@ class TimelineModifier {
     this.colorTimeline(isFirst);
   }
 
+  // Add month/date to timestamp.
   addDate() {
     function pad2(n) {
       return n.toString().padStart(2, "0");
     }
 
+    // Apply to all timestamps, including items inside summary (a summary can
+    // contain mutliple timestamps).
     const timestamps = this.list.getElementsByClassName("mx_MessageTimestamp");
     for (const timestamp of timestamps) {
       const date = new Date(timestamp.getAttribute("title"));
@@ -232,58 +273,79 @@ class TimelineModifier {
     }
   }
 
+  // Apply alternating line coloring.
   colorTimeline(isFirst) {
-    const items = this.getItems();
+    const items = this.collectItems();
     const firstItemIsOdd = isFirst ? false : this.isFirstItemOdd(items);
     this.noteOddEven(firstItemIsOdd, items);
     this.setClass(firstItemIsOdd, items);
   }
 
-  getItems() {
+  // Returns items for alternating line coloring.
+  collectItems() {
     const items = [];
     for (const item of this.list.childNodes) {
       const nodeName = item.nodeName.toLowerCase();
       if (nodeName === "li") {
         if (item.classList.contains("mx_RoomView_myReadMarker_container")) {
+          // Marker for new message.
           continue;
         }
+
+        // Message or separator for "Today" etc.
       } else if (nodeName === "div") {
         if (!item.classList.contains("mx_EventListSummary")) {
+          // Typing notification.
           continue;
         }
+
+        // Event summary (joined, changed name, etc).
+        // This can contain multiple "li", but given it's expandable,
+        // apply single color to the entire summary.
       } else {
         continue;
       }
 
+      // Message contains token.
       const token = item.getAttribute("data-scroll-tokens");
+
       items.push({ node: item, token });
     }
+
     return items;
   }
 
+  // Calculate the `odd` flag for the first line, after items inside the
+  // timeline are modified.
   isFirstItemOdd(items) {
-    let firstItemIsOdd = false;
     for (let i = 0; i < items.length; i++) {
       const item = items[i];
 
       if (item.token in this.oddEvenMap) {
-        firstItemIsOdd = this.oddEvenMap[item.token]
+        return this.oddEvenMap[item.token]
           ? !(i % 2)
           : !!(i % 2);
-        break;
       }
     }
-    return firstItemIsOdd;
+
+    return false;
   }
 
+  // Set class for alternating line coloring, used by CSS.
   setClass(odd, items) {
     for (const item of items) {
+      // Basically this shouldn't be necessary, unless the coloring gets
+      // *flipped*.
       item.node.classList.remove(odd ? "ext-line-even" : "ext-line-odd");
+
       item.node.classList.add(odd ? "ext-line-odd" : "ext-line-even");
       odd = !odd;
     }
   }
 
+  // Note the current `odd` flag of some items, to keep coloring not *flipped*
+  // when new items are added to timeline.
+  // The data is use by isFirstItemOdd.
   noteOddEven(firstItemIsOdd, items) {
     const noteOne = (index, token) => {
       const odd = firstItemIsOdd
@@ -310,15 +372,14 @@ class TimelineModifier {
       }
     }
   }
-
-  clearOddEvenMap() {
-    this.oddEvenMap = {};
-  }
 }
 
 async function onLoad() {
+  const themeToClass = new ThemeToClass();
+  await themeToClass.apply();
+
   const modifier = new TimelineModifier();
-  modifier.run().catch(e => console.error(e));
+  await modifier.apply();
 }
 
 onLoad().catch(e => console.error(e));
